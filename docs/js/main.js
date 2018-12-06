5d59eb4a539568661c60f313488cffe32d0763c5
Marcin Górnikowski <bokserkabaty@gmail.com>
2018-12-06 04:59:31 +0100

    adjust counter to count how much time is ladder up after exceeding 8dec 2am date

--- docs/js/main.js
+++ docs/js/main.js
@@ -12,7 +12,7 @@ $(function () {
 	new WOW().init();
 
   // 
-  ////// Count down timer
+  ////// Countdown timer
   //
 
   // Set the date we're counting down to
@@ -23,26 +23,20 @@ $(function () {
 
     // Get todays date and time
     let now = new Date().getTime();
-
-    // Find the difference between now and the count down date
     let difference = countDownDate - now;
 
     // Time calculations for days, hours, minutes and seconds
-    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+    const days = Math.abs(Math.floor(difference / (1000 * 60 * 60 * 24)));
+    const hours = Math.abs(Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const minutes = Math.abs(Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)));
+    const seconds = Math.abs(Math.floor((difference % (1000 * 60)) / 1000));
 
-    // Display the result in the element with id="demo"
-    $("#counter")
-      .html(`Pozostało: <span class="mx-1 mx-md-3">${days} dni</span><span class="mr-1 mr-md-3">${hours} godz</span><span class="mr-1 mr-md-3">${minutes} min</span><span>${seconds} sek</span>`);
-
-
-
-    // If the count down is finished, write some text 
-    if (difference < 0) {
-      clearInterval(x);
-      $("#counter").html("LADDER LIVE");
+    if (difference > 0) {
+      $("#counter").html(`Pozostało: <span class="mx-1 mx-md-3">${days} dni</span><span class="mr-1 mr-md-3">${hours} godz</span><span class="mr-1 mr-md-3">${minutes} min</span><span>${seconds} sek</span>`);
+    } else {
+      difference = now - countDownDate;
+      $("#counter").html(`Ladder od: <span class="mx-1 mx-md-3">${days} dni</span><span class="mr-1 mr-md-3">${hours} godz</span><span class="mr-1 mr-md-3">${minutes} min</span><span>${seconds} sek</span>`);
     }
+    
   }, 1000);
 });
