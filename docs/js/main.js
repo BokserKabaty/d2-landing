7a8b783729b5726f174a4a0ee24f60e4dd2c276f
Marcin Górnikowski <bokserkabaty@gmail.com>
2018-12-16 21:43:28 +0100

    repair timer while ladder is active

--- docs/js/main.js
+++ docs/js/main.js
@@ -30,10 +30,10 @@ $(function () {
     let difference = countDownDate - now;
 
     // Time calculations for days, hours, minutes and seconds
-    const days = Math.abs(Math.floor(difference / (1000 * 60 * 60 * 24)));
-    const hours = Math.abs(Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-    const minutes = Math.abs(Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)));
-    const seconds = Math.abs(Math.floor((difference % (1000 * 60)) / 1000));
+    const days = Math.abs(Math.ceil(difference / (1000 * 60 * 60 * 24)));
+    const hours = Math.abs(Math.ceil((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const minutes = Math.abs(Math.ceil((difference % (1000 * 60 * 60)) / (1000 * 60)));
+    const seconds = Math.abs(Math.ceil((difference % (1000 * 60)) / 1000));
 
     if (difference > 0) {
       $("#counter").html(`Pozostało: <span class="mx-1 mx-md-3">${days} dni</span><span class="mr-1 mr-md-3">${hours} godz</span><span class="mr-1 mr-md-3">${minutes} min</span><span>${seconds} sek</span>`);
