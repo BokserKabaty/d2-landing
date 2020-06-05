909cee40f976f991920e5fb022ccfb717cd980a4
Marcin Górnikowski <bokserkabaty@gmail.com>
2020-06-05 05:12:09 +0200

    fixed day's counter display

--- docs/js/main.js
+++ docs/js/main.js
@@ -34,7 +34,8 @@ $(function () {
     let difference = countDownDate - now;
 
     // Time calculations for days, hours, minutes and seconds
-    const days = Math.abs(Math.ceil(difference / (1000 * 60 * 60 * 24)));
+    const days = Math.abs(Math.floor(difference / (1000 * 60 * 60 * 24)));
+    const days1 = Math.abs(Math.ceil(difference / (1000 * 60 * 60 * 24)));
     const hours = Math.abs(Math.ceil((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
     const minutes = Math.abs(Math.ceil((difference % (1000 * 60 * 60)) / (1000 * 60)));
     const seconds = Math.abs(Math.ceil((difference % (1000 * 60)) / 1000));
@@ -43,7 +44,7 @@ $(function () {
       $("#counter").html(`Pozostało: <span class="mx-1 mx-md-3">${days} dni</span><span class="mr-1 mr-md-3">${hours} godz</span><span class="mr-1 mr-md-3">${minutes} min</span><span>${seconds} sek</span>`);
     } else {
       difference = now - countDownDate;
-      $("#counter").html(`Ladder od: <span class="mx-1 mx-md-3">${days} dni</span><span class="mr-1 mr-md-3">${hours} godz</span><span class="mr-1 mr-md-3">${minutes} min</span><span>${seconds} sek</span>`);
+      $("#counter").html(`Ladder od: <span class="mx-1 mx-md-3">${days1} dni</span><span class="mr-1 mr-md-3">${hours} godz</span><span class="mr-1 mr-md-3">${minutes} min</span><span>${seconds} sek</span>`);
     }
     
   }, 1000);
